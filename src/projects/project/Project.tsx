86ced7153a27a7af4548c02e0245b66440a1bd5b
Anton <shurmanov_anton@rambler.ru>
2023-05-28 18:52:59 +0300

    mixins and variables

--- src/projects/project/Project.tsx
+++ src/projects/project/Project.tsx
@@ -1,18 +1,32 @@
 import React from 'react';
-import s from './Project.module.css'
+import s from './Project.module.scss'
 
 type ProjectsPropsType = {
     title: string
     description: string
+    style: TodolistImagePropsType
 }
+
+type TodolistImagePropsType = {
+    backgroundImage: string
+}
+
 export const Project = (props: ProjectsPropsType) => {
+
+    const onClickHandler = () => {
+
+    }
+
     return (
         <div className={s.project}>
-                <div className={s.pictureProject}>
-                    <button className={s.button}>Смотреть</button>
-                </div>
-                <h3 className={s.title}>{props.title}</h3>
+            <div className={s.pictureProject} style={props.style}>
+                <button onClick={onClickHandler} className={s.button}>Смотреть</button>
+            </div>
+            <div className={s.projectInfo}>
+                <h4 className={s.projectTitle}>{props.title}</h4>
                 <span className={s.description}>{props.description}</span>
+            </div>
+
         </div>
     );
 };
--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -2,16 +2,20 @@ import React from 'react';
 import s from './Skills.module.css'
 import {Skill} from "./skill/Skill";
 import styleContainer from "../common/styles/Container.module.css";
+import {Title} from "../common/components/title/Title";
 
 export const Skills = () => {
     return (
         <div className={s.skillsBlock}>
-            <div className= {`${styleContainer.container} ${s.skillsContainer}`}  >
-                <h2 className={s.title}>My skills</h2>
+            <div className={`${styleContainer.container} ${s.skillsContainer}`}>
+                <Title title={'My skills'}/>
                 <div className={s.skills}>
-                    <Skill description={'Lorem Ipsum не только успешно пережил без заметных изменений пять веков.'} title={'HTML&CSS'}/>
-                    <Skill description={'Многие думают, что Lorem Ipsum - взятый с потолка псевдо-латинский набор. '} title={'JS'}/>
-                    <Skill description={'Если вам нужен Lorem Ipsum для серьёзного проекта, вы наверняка не хотите.'} title={'React'}/>
+                    <Skill description={'Lorem Ipsum не только успешно пережил без заметных изменений пять веков.'}
+                           title={'HTML&CSS'}/>
+                    <Skill description={'Многие думают, что Lorem Ipsum - взятый с потолка псевдо-латинский набор. '}
+                           title={'JS'}/>
+                    <Skill description={'Если вам нужен Lorem Ipsum для серьёзного проекта, вы наверняка не хотите.'}
+                           title={'React'}/>
                 </div>
             </div>
         </div>
