13004dc9045a47d05a114a6c3c6b8e59c324a738
Anton <shurmanov_anton@rambler.ru>
2023-06-18 15:48:29 +0300

    Main block scss

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import s from './Main.module.css';
+import s from './Main.module.scss';
 import styleContainer from './../common/styles/Container.module.css'
+import myAva2 from '../assets/images/myAva2.jpg'
 
 export const Main = () => {
+
     return (
         <div className={s.mainBlock}>
             <div className={styleContainer.container}>
                 <div className={s.text}>
-                    <span>Hi There</span>
-                    <h1>I am Anton Shurmanov</h1>
-                    <p>Frontend Developer.</p>
+                    {/*<span>Hi</span>*/}
+                    <h2>Hi, I'm Anton Shurmanov</h2>
+                    <h1>I'm a Developer</h1>
                 </div>
-                <div className={s.photo}></div>
+                <div className={s.photo}>
+                    <img src={myAva2} alt={''}/>
+                </div>
             </div>
         </div>
     );
