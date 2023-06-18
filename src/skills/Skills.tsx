173db2f50ad8d54a6e0e9aa9d13386b08f025bb3
Anton <shurmanov_anton@rambler.ru>
2023-06-18 17:09:35 +0300

    skills block scss

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -4,17 +4,29 @@ import {Skill} from "./skill/Skill";
 import styleContainer from "../common/styles/Container.module.css";
 import {Title} from "../common/components/title/Title";
 
+export type ProgressStyleType = {
+    htmlProgress:string
+    jsProgress:string
+    reactProgress:string
+}
 export const Skills = () => {
+
+    const progressStyle: ProgressStyleType = {
+        htmlProgress : '40%',
+        jsProgress : '80%',
+        reactProgress : '70%',
+    }
+
     return (
         <div className={s.skillsBlock}>
             <div className={`${styleContainer.container} ${s.skillsContainer}`}>
                 <Title title={'My skills'}/>
                 <div className={s.skills}>
-                    <Skill description={'Lorem Ipsum не только успешно пережил без заметных изменений пять веков.'}
+                    <Skill progressStyle={progressStyle.htmlProgress} description={'Lorem Ipsum не только успешно пережил без заметных изменений пять веков.'}
                            title={'HTML&CSS'}/>
-                    <Skill description={'Многие думают, что Lorem Ipsum - взятый с потолка псевдо-латинский набор. '}
+                    <Skill progressStyle={progressStyle.jsProgress} description={'Многие думают, что Lorem Ipsum - взятый с потолка псевдо-латинский набор. '}
                            title={'JS'}/>
-                    <Skill description={'Если вам нужен Lorem Ipsum для серьёзного проекта, вы наверняка не хотите.'}
+                    <Skill progressStyle={progressStyle.reactProgress} description={'Если вам нужен Lorem Ipsum для серьёзного проекта, вы наверняка не хотите.'}
                            title={'React'}/>
                 </div>
             </div>
--- src/skills/skill/Skill.tsx
+++ src/skills/skill/Skill.tsx
@@ -2,15 +2,27 @@ import React from 'react';
 import s from './Skill.module.scss'
 
 
+
 type SkillPropsType = {
     title:string
     description:string
+    progressStyle:string
 }
 export const Skill = (props:SkillPropsType) => {
+
+    const progress = {
+        width: props.progressStyle
+    }
+
     return (
         <div className={s.skill}>
-            <div className={s.icon}></div>
             <h3 className={s.title}>{props.title}</h3>
+            <div className={s.icons}>
+                <div className={s.icon} style={progress}></div>
+                <div className={s.progress}>{props.progressStyle}</div>
+            </div>
+
+
             <span className={s.description}>{props.description} </span>
         </div>
     );
